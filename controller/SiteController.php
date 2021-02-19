ccb3b5891c4aafe457297a440632acaea1b2601e
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-19 10:42:15 +0200

    add param Request to callUserFunc and receive it in handle contact

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -6,6 +6,7 @@ namespace app\controller;
 
 use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
 
 class SiteController extends Controller
 {
@@ -51,10 +52,10 @@ class SiteController extends Controller
      *
      * @return string
      */
-    public function handleContact()
+    public function handleContact(Request $request)
     {
         // we user get body method to see user input
-        $body = Application::$app->request->getBody();
+        $body = $request->getBody();
 
         echo "<pre>";
         var_dump($body);
