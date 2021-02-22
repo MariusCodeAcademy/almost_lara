c89a919ca86a0330c0f13e7177b49e4ee08d8af6
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-22 11:45:47 +0200

    add UserModel.php to AuthController.php and use it to register

--- core/AuthController.php
+++ core/AuthController.php
@@ -3,6 +3,8 @@
 
 namespace app\core;
 
+use app\model\UserModel;
+
 /**
  * Respossible for handling login and register.
  *
@@ -12,10 +14,12 @@ namespace app\core;
 class AuthController extends Controller
 {
     public Validation $vld;
+    protected UserModel $userModel;
 
     public function __construct()
     {
         $this->vld = new Validation();
+        $this->userModel = new UserModel();
     }
 
     public function login()
@@ -77,7 +81,7 @@ class AuthController extends Controller
                     // set flash msg
 //                    flash('register_success', 'You have registered successfully');
                     // header("Location: " . URLROOT . "/users/login");
-                    redirect('/login');
+//                    redirect('/login');
                 } else {
                     die('Something went wrong in adding user to db');
                 }
