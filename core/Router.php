06f831d34e86725ee5d791499e737d63bbcd0acd
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-24 08:53:18 +0200

    trim /post/{id} to /post and pass id as argname in callback

--- core/Router.php
+++ core/Router.php
@@ -55,15 +55,15 @@ class Router
             $startPos = strpos($path, '{');
             $endPos = strpos($path, '}');
             $argName = substr($path, $startPos + 1, $endPos - $startPos - 1);
-            $callback[] = $argName;
-            $path = substr($path, $startPos -1);
+            $callback['urlParamName'] = $argName;
+            $path = substr($path, 0, $startPos -1);
 
 //        echo "<pre>";
-//
 //        var_dump($path);
-//        var_dump($this->routes);
+////        var_dump($this->routes);
 //        echo "</pre>";
 //        exit;
+
         endif;
 
 
@@ -89,14 +89,17 @@ class Router
         $path = $this->request->getPath();
         $method = $this->request->method();
 
-
+//        echo "<pre>";
+//        var_dump($path);
+//        echo "</pre>";
+//        exit;
 
 
         // trying to run a route from routes array
         $callback = $this->routes[$method][$path] ?? false;
 
 //        echo "<pre>";
-//        var_dump($this->routes);
+//        print_r($this->routes);
 //        echo "</pre>";
 //        exit;
 
@@ -118,11 +121,22 @@ class Router
             $instance = new $callback[0];
             Application::$app->controller = $instance;
             $callback[0] = Application::$app->controller;
+
+            // check if we have url arguments in callback array
+            if (isset($callback['urlParamName'])) :
+                //     [0] => app\controller\PostsController
+//                    [1] => post
+//                    [urlParamName] => id
+                $urlParamName = $callback['urlParamName'];
+                // make call back array with 2 members
+                array_splice($callback, 2, 1);
+            endif;
+
         endif;
 
 
         // page dose exsist we call user function
-        return call_user_func($callback, $this->request);
+        return call_user_func($callback, $this->request, $urlParamName ?? null);
 
     }
 
